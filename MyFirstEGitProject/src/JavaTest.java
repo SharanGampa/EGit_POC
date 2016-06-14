3fe2c22b652b98dbf2be8112266df0ddb345dd01
335134 <Sharan.Gampa@cognizant.com>
2016-06-14 16:19:39 +0530

    Revert "chumma commit"
    This reverts commit 30f7f4e3038a2706fa42f94c24ff04e2fd1c9ca9.

---
+++ MyFirstEGitProject/src/JavaTest.java
@@ -0,0 +1,18 @@
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("How are you??");
+		System.out.println("I am good!!!");
+		System.out.println("Am following correct???");
+		System.out.println("Trying for sync.");
+		System.out.println("still not underdtood completly.");
+		System.out.println("After vedio tutorial.");
+		System.out.println("After vedio tutorial 2.");
+		System.out.println("After vedio tutorial 3.");
+		System.out.println("removed last two lines :P");
+	}
+
+}
